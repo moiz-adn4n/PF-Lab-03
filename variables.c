de13777243d285949fce67bca59e72ff8fd39ff4
moiz-adn4n <k256584@nu.edu.pk>
2026-02-08 20:00:49 +0500

    Add variables and data types program

---
+++ variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int i = 10;
+    float f = 3.14f;
+    double d = 9.8765;
+    char c = 'A';
+
+    printf("Integer: %d\n", i);
+    printf("Float: %.2f\n", f);
+    printf("Double: %lf\n", d);
+    printf("Character: %c\n", c);
+
+    return 0;
+}
